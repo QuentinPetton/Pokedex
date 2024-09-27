5d7326f6d3c84d990d1016e7ec87e67263d96dd5
QuentinPetton <quentin.petton@hotmail.fr>
2024-09-27 11:23:29 +0200

    ajout configuraiton pour deployement projet

--- back/models/sequelizeClient.js
+++ back/models/sequelizeClient.js
@@ -4,7 +4,20 @@ import { config } from "dotenv";
 import { Sequelize } from "sequelize";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-config({ path: resolve(__dirname, "../..", ".env") });
+
+// Charger dotenv uniquement en développement local
+if (process.env.NODE_ENV === "dev") {
+  config({ path: resolve(__dirname, "../..", ".env") });
+}
+
+const databaseUrl = process.env.PG_URL;
+
+if (!databaseUrl) {
+  throw new Error('Database URL is not defined. Check your environment variables.');
+}
+
+
+
 
 
 export const sequelize = new Sequelize(process.env.PG_URL, {
